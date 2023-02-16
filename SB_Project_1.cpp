4b7c0d1828009034ee8068051bae932d007b6f2c
Krager <kragomor@mail.ru>
2023-02-16 21:29:41 +0300

    V-1.2

--- SB_Project_1.cpp
+++ SB_Project_1.cpp
@@ -1,30 +1,32 @@
 ﻿#include <iostream>
+#include <ctime>
+
 using namespace std;
-void PrintNumbers(bool isOdd, int N)
-
-{
-	for (int i = 0; i <= N; i++) {
-		if (isOdd) {
-			if (i % 2 > 0) {
-				cout << i << "\n";
-			}
-		}
-		else {
-			if (i % 2 == 0) {
-				cout << i << "\n";
-			}
-		}
-
-	}
-	//for (int i = isOdd, i <= N, i+=2) { -  альтернативный укороченый вариант, завязанный на счетчике круга.
-	// cout << i ;}
-
-	}
 
 int main()
 {
-	PrintNumbers (false, 10);
-	PrintNumbers (true, 10);
+	struct tm newtime;
+	time_t now = time(0);
+	localtime_s(&newtime, &now);
+	int Day = newtime.tm_mday;
 	
-		return 0;
-}
+	const int N = 5;
+	int array[N][N]; 
+	
+	for (int i = 0; i < N; i++) 
+	{
+		for (int j = 0; j < N; j++) 
+		{
+			array[i][j] = i + j;
+			cout << array[i][j];
+		}
+		cout << endl;
+
+	}
+
+	int i = (Day % N) + 1;
+	int sum = array[i][0] + array[i][1] + array[i][2] + array[i][3] + array[i][4];
+	
+	cout << Day << endl;
+	cout << sum << endl;
+}	
