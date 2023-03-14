27398f0c81ec815533cd34077723b84e60eb84d0
Krager <kragomor@mail.ru>
2023-03-14 22:38:29 +0300

    V-1.6

--- SB_Project_1.cpp
+++ SB_Project_1.cpp
@@ -1,87 +1,58 @@
 ﻿#include <iostream>
 #include <string>
-//#include <bits/stdc++.h>
+
 using namespace std;
 
 
-class player 
+class ANIMAL 
 {
-//private:
-//	string name;
-//	int score;
-
 public:
-	string name;
-	int score;
-
-	int Getscore()
-	{
-		return score;
-	}
-
-	void Enter ()
-	{
-		cout << "Введите имя игрока -" << endl;
-		cin >> name;
-		cout << "Введите количество очков -" << endl;
-		cin >> score;
-	}
 	
-	void print()
+	virtual void voice()
 	{
-		cout << "Имя игрока - " << name << "\t" << "Очки игрока -" << score << endl;
-
+		cout << "....." << endl;
 	}
-			
 };
 
-
-void swap(player& p1, player& p2)
+class DOG : public ANIMAL
 {
-	player temp = p1;
-	p1 = p2;
-	p2 = temp;
-
-}
-void bubbleSort(player arr[], int size)
-{
-	for (int i = 0; i < size - 1; i++)
+public:
+	void voice() override
 	{
-		for (int j = 0; j < size - i - 1; j++)
-		{
-			if (arr[j].score > arr[j + 1].score)
-			{
-				swap(arr[j], arr[j + 1]);
-
-			}
-		}
-
+		cout << "Woof" << endl;
 	}
-}
+};
+class CAT : public ANIMAL
+{
+public:
+	void voice() override
+	{
+		cout << "Mur" << endl;
+	}
+};
+class BEAR : public ANIMAL
+{
+public:
+	void voice() override
+	{
+		cout << "Arrr" << endl;
+	}
+};
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	int size;
-	cout << "Введите количество игроков -" << endl;
-	cin >> size;
 
-	player* Array = new player[size];
+    ANIMAL* Array[3] = { new DOG, new CAT, new BEAR };
 
-	for (int x = 0; x < size; x++)
+	for (int i = 0; i < 3; i++)
 	{
-		Array[x].Enter();
+		Array[i]->voice();
 
 	}
-	bubbleSort (Array, size);
-	
-	for (int x = 0; x < size; x++)
-	{
-		Array[x].print();
 
-	}
-	
 	delete[] Array;
+
 
 }
 
